501f47a347528fc1fc0e05e5dbb3a6b27f374a7a
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-15 17:07:08 +0600

    added backround work

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import asyncio
 from app.bot import dp, bot
 from app.config import load_config
 from app.utils.logger import logger
-
 from app.handlers import start, weather, forecast, help
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import threading
+
+
+#Большинство кода здесь для того чтобы он работал
+# на сервере, потому что на сервер который я задеплоил нужен
+# что то вроде backroud work с портом
+def run_server():
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("Starting HTTP server on port 8080...")
+    httpd.serve_forever()
 
 async def main():
+    server_thread = threading.Thread(target=run_server)
+    server_thread.daemon = True
+    server_thread.start()
+
     config = load_config()
     logger.info("Starting bot")
     try:
