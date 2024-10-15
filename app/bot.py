0db7af36b13f0d89f7affa7c3a5c188f9766bb69
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-15 16:47:47 +0600

    added proxy

--- app/bot.py
+++ app/bot.py
@@ -2,5 +2,5 @@ from aiogram import Bot, Dispatcher
 from app.config import load_config
 
 config = load_config()
-bot = Bot(token=config.TELEGRAM_BOT_TOKEN)
+bot = Bot(token=config.TELEGRAM_BOT_TOKEN, proxy='http://proxy.server:3128')
 dp = Dispatcher()
